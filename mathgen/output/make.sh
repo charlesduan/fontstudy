d18cd93e4ad2f9a983bc2a2b4c070344bb423ba6
Charles Duan <cduan@sbf5.com>
2015-02-02 02:51:58 -0500

    Improved bold fonts; fixed other bugs

--- mathgen/output/make.sh
+++ mathgen/output/make.sh
@@ -3,11 +3,11 @@
 $FAMILY="Century Expanded CD"
 $NAME="CenturyExpandedCD"
 
-mf cenexpbi && gftodvi cenexpbi.2602gf && dvipdf cenexpbi
-mf2pt1 --encoding=cdlatin1.enc \
-    cenexpbi
-./fixfont.pe cenexpbi.pfb Bold Italic
-fontforge -script fixfont.py cenexpbi.otf cenexpbi.otf
+#mf cenexpbi && gftodvi cenexpbi.2602gf && dvipdf cenexpbi
+#mf2pt1 --encoding=cdlatin1.enc \
+#    cenexpbi
+#./fixfont.pe cenexpbi.pfb Bold Italic
+#fontforge -script fixfont.py cenexpbi.otf cenexpbi.otf
 
 mf cenexpb && gftodvi cenexpb.2602gf && dvipdf cenexpb
 mf2pt1 --encoding=cdlatin1.enc \
@@ -15,14 +15,14 @@ mf2pt1 --encoding=cdlatin1.enc \
 ./fixfont.pe cenexpb.pfb Bold ''
 fontforge -script fixfont.py cenexpb.otf cenexpb.otf
 
-mf cenexp && gftodvi cenexp.2602gf && dvipdf cenexp
-mf2pt1 --encoding=cdlatin1.enc \
-    cenexp
-./fixfont.pe cenexp.pfb Regular ''
-fontforge -script fixfont.py cenexp.otf cenexp.otf
+#mf cenexp && gftodvi cenexp.2602gf && dvipdf cenexp
+#mf2pt1 --encoding=cdlatin1.enc \
+#    cenexp
+#./fixfont.pe cenexp.pfb Regular ''
+#fontforge -script fixfont.py cenexp.otf cenexp.otf
 
-mf cenexpi && gftodvi cenexpi.2602gf && dvipdf cenexpi
-mf2pt1 --encoding=cdlatin1.enc \
-    cenexpi
-./fixfont.pe cenexpi.pfb Regular Italic
-fontforge -script fixfont.py cenexpi.otf cenexpi.otf
+#mf cenexpi && gftodvi cenexpi.2602gf && dvipdf cenexpi
+#mf2pt1 --encoding=cdlatin1.enc \
+#    cenexpi
+#./fixfont.pe cenexpi.pfb Regular Italic
+#fontforge -script fixfont.py cenexpi.otf cenexpi.otf
