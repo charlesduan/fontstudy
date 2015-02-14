8fa58984eb4ed89efeb1e28ba422543ac680b6bd
Charles Duan <cduan@sbf5.com>
2015-02-13 22:57:50 -0500

    Improved kerning

--- mathgen/output/fixfont.py
+++ mathgen/output/fixfont.py
@@ -18,11 +18,13 @@ font.addLookupSubtable("Kern lookup", "Kern lookup 0")
 lc_list = [ string.ascii_lowercase[x:x+1] for x in range(26) ] + \
         [ 'ff', 'ffi', 'ffl', 'fi', 'fl' ]
 uc_left_list = [
-        'A', 'C', 'D', 'G', 'L', 'O', 'P', 'Q', 'R', 'S', 'T', 'U',
+        'A', 'C', 'D', 'G', 'O', 'Q', 'R', 'S', 'U',
         'V', 'W', 'Y' ]
+uc_less_left_list = [ 'L', 'P', 'T' ]
 uc_right_list = [ 'A', 'C', 'G', 'J', 'O', 'Q', 'T', 'U', 'V', 'W', 'Y' ]
 
 sc_left_list = [ x.lower() + ".sc" for x in uc_left_list ]
+sc_less_left_list = [ x.lower() + ".sc" for x in uc_less_left_list ]
 sc_right_list = [ x.lower() + ".sc" for x in uc_right_list ]
 
 lc_left_list = [
@@ -40,10 +42,10 @@ sc_list = [ string.ascii_lowercase[x:x+1] + ".sc" for x in range(26) ]
 lpunct_list = [ 'quoteleft', 'quotedblleft' ]
 rpunct_list = [ 'quoteright', 'quotedblright', 'period', 'comma' ]
 
-font.autoKern("Kern lookup 0", 325, uc_left_list, uc_right_list, 50,
-        onlyCloser=True)
-font.autoKern("Kern lookup 0", 250, sc_left_list, sc_right_list, 50,
-        onlyCloser=True)
+font.autoKern("Kern lookup 0", 325, uc_left_list + uc_less_left_list,
+        uc_right_list, 50, onlyCloser=True)
+font.autoKern("Kern lookup 0", 250, sc_left_list + sc_less_left_list,
+        sc_right_list, 50, onlyCloser=True)
 font.autoKern("Kern lookup 0", 250, uc_top_list, lc_short_list + sc_list, 50,
         onlyCloser=True)
 
@@ -52,7 +54,11 @@ font.autoKern("Kern lookup 0", 300, lpunct_list, uc_right_list,
 font.autoKern("Kern lookup 0", 200, lpunct_list,
         sc_right_list + lc_short_list,
         50, onlyCloser=True)
+font.autoKern("Kern lookup 0", 350, uc_less_left_list, rpunct_list,
+        50, onlyCloser=True)
 font.autoKern("Kern lookup 0", 250, uc_left_list, rpunct_list,
+        50, onlyCloser=True)
+font.autoKern("Kern lookup 0", 300, sc_less_left_list, rpunct_list,
         50, onlyCloser=True)
 font.autoKern("Kern lookup 0", 200, lc_left_list +
         lc_left_punct_list + sc_left_list, rpunct_list,
