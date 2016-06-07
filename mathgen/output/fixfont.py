67a108560de30acf92e5b6ec6573a1a0bc670072
Charles Duan <cduan@sbf5.com>
2016-06-07 01:54:45 -0400

    Improved kerning

--- mathgen/output/fixfont.py
+++ mathgen/output/fixfont.py
@@ -33,14 +33,18 @@ lc_left_punct_list = [ 'g', 'h', 'k', 'm', 'n', 's', 't', 'u', 'x', 'z' ]
 lc_right_list = [
         'a', 'c', 'd', 'e', 'j', 'o', 'q', 't', 'u', 'v', 'w', 'y' ]
 
-uc_top_list = [ 'F', 'P', 'T', 'V', 'W', 'Y' ]
+uc_top_list = [ 'F', 'P', 'V', 'W', 'Y' ]
+uc_top_more_list = [ 'U', 'J', 'T' ]
+uc_top_more_left_list = [ 'J', 'N' ]
+sc_top_list = [ x.lower() + ".sc" for x in (uc_top_list + uc_top_more_list) ]
 lc_short_list = [ 'a', 'c', 'd', 'e', 'g', 'i', 'j', 'm', 'n', 'o', 'q', 'r',
         's', 'u', 'v', 'w', 'x', 'y', 'z' ]
 
 sc_list = [ string.ascii_lowercase[x:x+1] + ".sc" for x in range(26) ]
 
 lpunct_list = [ 'quoteleft', 'quotedblleft' ]
-rpunct_list = [ 'quoteright', 'quotedblright', 'period', 'comma' ]
+rpunct_list = [ 'quoteright', 'quotedblright' ]
+lowpunct_list = [ 'period', 'comma' ]
 
 font.autoKern("Kern lookup 0", 325, uc_left_list + uc_less_left_list,
         uc_right_list, 50, onlyCloser=True)
@@ -58,12 +62,56 @@ font.autoKern("Kern lookup 0", 200, lpunct_list,
         50, onlyCloser=True)
 font.autoKern("Kern lookup 0", 350, uc_less_left_list, rpunct_list,
         50, onlyCloser=True)
-font.autoKern("Kern lookup 0", 250, uc_left_list, rpunct_list,
+font.autoKern("Kern lookup 0", 250, uc_left_list + uc_top_list, rpunct_list,
         50, onlyCloser=True)
 font.autoKern("Kern lookup 0", 300, sc_less_left_list, rpunct_list,
         50, onlyCloser=True)
 font.autoKern("Kern lookup 0", 200, lc_left_list +
         lc_left_punct_list + sc_left_list, rpunct_list,
         50, onlyCloser=True)
+font.autoKern("Kern lookup 0", 100, [ 'f', 'f_f' ], rpunct_list,
+        50, onlyCloser=False)
+
+font.autoKern("Kern lookup 0", 200, lc_left_list,
+        lowpunct_list, 50, onlyCloser=True)
+font.autoKern("Kern lookup 0", 300, [ 'F', 'V', 'P' ], lowpunct_list,
+        50, onlyCloser=True)
+font.autoKern("Kern lookup 0", 250, [ 'W' ], lowpunct_list,
+        50, onlyCloser=True)
+font.autoKern("Kern lookup 0", 200, [ 'T', 'Y' ], lowpunct_list,
+        50, onlyCloser=True)
+font.autoKern("Kern lookup 0", 150, [ 'J', 'N', 'U' ], lowpunct_list,
+        50, onlyCloser=True)
+font.autoKern("Kern lookup 0", 150, lowpunct_list, uc_top_more_list,
+        50, onlyCloser=True)
+
+font.autoKern("Kern lookup 0", 300, lowpunct_list, [ 'V' ],
+        50, onlyCloser=True)
+font.autoKern("Kern lookup 0", 250, lowpunct_list, [ 'W' ],
+        50, onlyCloser=True)
+font.autoKern("Kern lookup 0", 200, lowpunct_list, [ 'T', 'Y' ],
+        50, onlyCloser=True)
+font.autoKern("Kern lookup 0", 150, lowpunct_list, [ 'U' ],
+        50, onlyCloser=True)
+
+font.autoKern("Kern lookup 0", 250, [ 'f.sc', 'p.sc', 'v.sc' ], lowpunct_list,
+        50, onlyCloser=True)
+font.autoKern("Kern lookup 0", 200, [ 'w.sc' ], lowpunct_list,
+        50, onlyCloser=True)
+font.autoKern("Kern lookup 0", 150,
+        [ 't.sc', 'y.sc', 'j.sc', 'u.sc' ], lowpunct_list,
+        50, onlyCloser=True)
+font.autoKern("Kern lookup 0", 120, [ 'n.sc' ], lowpunct_list,
+        50, onlyCloser=True)
+
+font.autoKern("Kern lookup 0", 250, lowpunct_list, [ 'v.sc' ],
+        50, onlyCloser=True)
+font.autoKern("Kern lookup 0", 200, lowpunct_list, [ 'w.sc' ],
+        50, onlyCloser=True)
+font.autoKern("Kern lookup 0", 150, lowpunct_list, [ 't.sc', 'y.sc', 'u.sc' ],
+        50, onlyCloser=True)
+
+font.autoKern("Kern lookup 0", 120, lowpunct_list, [ 'J', 'j.sc' ],
+        50, onlyCloser=False)
 
 font.generate(outfile)
